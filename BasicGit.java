f7138537fa7e7b3bf975971c9f5e44c17a9f323d
Solomon Astley <solastley@gmail.com>
2017-10-16 13:15:06 -0400

    final

---
+++ BasicGit.java
@@ -0,0 +1,9 @@
+// prints the numbers from 1 - 10 inclusive
+
+public class BasicGit {
+  public static void main(String [] args) {
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(i);
+    }
+  }
+}
